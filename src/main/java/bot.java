44adcac3edd63c4fba9e504a3dd649144eb2c18b
WayvshockGD <61085765+WayvshockGD@users.noreply.github.com>
2020-10-13 07:21:58 -0400

    Update bot.java

--- src/main/java/bot.java
+++ src/main/java/bot.java
@@ -11,12 +11,16 @@ import java.awt.*;
 
 
 public class bot extends ListenerAdapter {
+	/*
+	 * prefix goes here. default = !
+	 */
+	public static String prefix = "|";
 
     public static void main(String[] args) throws LoginException {
-        String token = "YOUR_BOT_TOKEN";
+        String token = "Bot-Token";
         JDABuilder builder = JDABuilder.createDefault(token);
         builder.addEventListeners(new bot());
-        builder.setActivity(Activity.playing("STATUS"));
+        builder.setActivity(Activity.playing("Half life " + prefix + "info"));
         builder.build();
     }
 
@@ -29,10 +33,7 @@ public class bot extends ListenerAdapter {
             return;
         }
         else {
-        	/* 
-        	 * prefix goes here because yes default is = ! 
-        	 */
-        	String prefix = "!";
+            
             if (event.getChannel().getType().name().equals("dm")) return;
 			if (event.getMessage().getContentRaw().toLowerCase().equals(prefix + "ping")) {
                 long ping = event.getJDA().getGatewayPing();
@@ -43,25 +44,25 @@ public class bot extends ListenerAdapter {
             	
             	EmbedBuilder embedInfo = new EmbedBuilder();
             	
-            	embedInfo.setColor(Color.blue);
+            	embedInfo.setColor(Color.orange);
             	embedInfo.setTitle("Info");
-            	embedInfo.addField("This bot is made out of java", "i made this bot out of java because i wanted to"
-            			+ "and because jda is awsome :sunglasses:", false);
-            	embedInfo.addField("Links:", "\nThe bots github repository\nhttps://github.com/WayvshockGD/First-java-discord-bot\n"
-            			+ "invite the bot to your server\nhttps://discord.com/api/oauth2/authorize?client_id=666480078498168833&permissions=8&scope=bot", false);
+            	embedInfo.addField("This bot is made out of java", "i made this bot out of java because i wanted to and because jda is awsome :sunglasses:", false);
+            	embedInfo.addField("How to Use Bot:", "More commands comming soon but for now just a basic ping and help command. to use the command" +
+            	" say " + prefix + "help or " + prefix + "commands", false);
+            	embedInfo.addField("Bot invite:", "[Click Here](https://discord.com/api/oauth2/authorize?client_id=666480078498168833&permissions=8&scope=bot)", false);
+            	embedInfo.addField("Github Repository:", "[click here](https://github.com/WayvshockGD/First-java-discord-bot)", false);
             	embedInfo.setThumbnail(event.getJDA().getSelfUser().getAvatarUrl());
             	embedInfo.setFooter("2020 made in JDA");
             	
             	 event.getChannel().sendMessage(embedInfo.build()).queue();
             }
-            if (event.getMessage().getContentRaw().toLowerCase().equals(prefix + "stats")) {
-            	string BotGuilds = event.getMessage()
-            	
+            if (event.getMessage().getContentRaw().toLowerCase().equals(prefix + "help")) {
+            	event.getChannel().sendMessage("help:\n core: " + prefix + "info, " + prefix + "ping").queue();
             }
 
             if (event.getMessage().getContentRaw().equals("<@!" + event.getJDA().getSelfUser().getId() + ">") ||
             event.getMessage().getContentRaw().equals("<@" + event.getJDA().getSelfUser().getId() + ">")) {
-                event.getChannel().sendMessage(":wave: | Hello <@!" + event.getAuthor().getId() + "> my prefix is" + prefix).queue();
+                event.getChannel().sendMessage(":wave: | Hello <@!" + event.getAuthor().getId() + "> my prefix is " + prefix).queue();
             }
         }
     }
